a5f170e48ab0964cee12077431b12ac185c53410
Marina Antonova <mrna.antonova@gmail.com>
2026-02-08 16:35:13 +0200

    Migrations

---
+++ FamilyFlow/Data/FamilyFlowDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FamilyFlow.Data
+{
+    public class FamilyFlowDbContext : IdentityDbContext
+    {
+        public FamilyFlowDbContext(DbContextOptions<FamilyFlowDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- FamilyFlow/Program.cs
+++ FamilyFlow/Program.cs
@@ -8,16 +8,16 @@ namespace FamilyFlow
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            string?  connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            builder.Services.AddDbContext<FamilyFlowDbContext>(options =>
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+                .AddEntityFrameworkStores<FamilyFlowDbContext>();
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
