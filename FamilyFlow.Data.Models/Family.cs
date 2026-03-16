9da1f208d398218eaafcd93fd863eaeacf11d2f1
Marina Antonova <mrna.antonova@gmail.com>
2026-03-16 13:51:32 +0200

    Add Family model

---
+++ FamilyFlow.Data.Models/Family.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using static FamilyFlow.GCommon.ValidationConstants.Family;
+
+namespace FamilyFlow.Data.Models
+{
+    public class Family
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(NameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        public virtual ICollection<FamilyMember> Members { get; set; } 
+            = new List<FamilyMember>();
+
+    }
+}
--- FamilyFlow.GCommon/ValidationConstants.cs
+++ FamilyFlow.GCommon/ValidationConstants.cs
@@ -2,6 +2,11 @@
 {
     public static class ValidationConstants
     {
+        public static class Family
+        {
+            public const int NameMinLength = 3;
+            public const int NameMaxLength = 50;
+        }
         public static class FamilyMember
         {
             public const int NameMinLength = 3;
